38bf0ff92d18e65a67fdad9e012da10616bb4c85
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2022-02-12 17:21:22 +0100

    Create css files

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+export const MainContainer = styled.div`
+  max-width: 1200px;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Picture = styled.img`
+  width: 300px;
+  heigth: 400px;
+`;
+
+export const MainInfo = styled.div``;
+
+export const Header = styled.div`
+  display: flex;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: black;
+  color: white;
+  margin-top: 20px;
+`;
---
+++ src/components/Name/Name.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h1`
+  text-transform: uppercase;
+  letter-spacing: 2px;
+`;
+export const Position = styled.h2`
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`;
