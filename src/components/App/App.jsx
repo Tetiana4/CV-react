6b23f9eee995db494e02ec39b98fe02eb65e8afd
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2023-06-16 12:37:29 +0200

    Update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
+import ReactSwitch from 'react-switch';
 import { Summary, Experience, Education, Works } from '../../views';
 import { TechSkills } from '../TechSkills';
 import { SoftSkills } from '../SoftSkills';
@@ -7,9 +9,10 @@ import { Name } from '../Name';
 import { Navigation } from '../Navigation';
 import { Contacts } from '../Contacts';
 import photo from '../../images/picture.jpg';
+import './App.css';
 import {
   Header,
-  MainContainer,
+  // MainContainer,
   Container,
   Picture,
   SkillsStyle,
@@ -17,62 +20,85 @@ import {
   Box,
 } from './App.styled';
 
+import { createContext } from 'react';
+
+export const ThemeContext = createContext(null);
+
 export const App = () => {
+  const [theme, setTheme] = useState('dark');
+  const toggleTheme = () => {
+    setTheme(current => (current === 'light' ? 'dark' : 'light'));
+  };
   const isDesktop = useMediaQuery({ minWidth: 1160 });
   const isTablet = useMediaQuery({ minWidth: 760, maxWidth: 1159 });
   const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 769 });
 
   return (
-    <MainContainer>
-      {isMobile && (
-        <>
-          <ContainerMobile>
-            <Picture src={photo} alt="my photo" />
-            <Name />
-            <Contacts />
-            <TechSkills />
-            <SoftSkills />
-          </ContainerMobile>
-        </>
-      )}
-      {isTablet && (
-        <>
-          <Container>
-            <Picture src={photo} alt="my photo" />
-            <Box>
+    <ThemeContext.Provider value={(theme, toggleTheme)}>
+      <div className="main" id={theme}>
+        <div className="switch">
+          <label className="label">
+            {theme === 'light' ? 'Light Mode' : 'Dark Mode'}
+          </label>
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={theme === 'dark'}
+            onColor="#000"
+            onHandleColor="#555"
+            offHandleColor="#fff"
+            activeBoxShadow="0 0 5px 5px #202142"
+          />
+        </div>
+        {isMobile && (
+          <>
+            <ContainerMobile>
+              <Picture src={photo} alt="my photo" />
               <Name />
               <Contacts />
-            </Box>
-          </Container>
-          <SkillsStyle>
-            <TechSkills />
-            <SoftSkills />
-          </SkillsStyle>
-        </>
-      )}
-
-      {isDesktop && (
-        <>
-          <Container>
-            <Picture src={photo} alt="my photo" />
-            <Box>
-              <Name />
-              <Header>
+              <TechSkills />
+              <SoftSkills />
+            </ContainerMobile>
+          </>
+        )}
+        {isTablet && (
+          <>
+            <Container>
+              <Picture src={photo} alt="my photo" />
+              <Box>
+                <Name />
                 <Contacts />
-                <TechSkills />
-                <SoftSkills />
-              </Header>
-            </Box>
-          </Container>
-        </>
-      )}
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Summary />} />
-        <Route path="projects" element={<Experience />} />
-        <Route path="education" element={<Education />} />
-        <Route path="experience" element={<Works />} />
-      </Routes>
-    </MainContainer>
+              </Box>
+            </Container>
+            <SkillsStyle>
+              <TechSkills />
+              <SoftSkills />
+            </SkillsStyle>
+          </>
+        )}
+
+        {isDesktop && (
+          <>
+            <Container>
+              <Picture src={photo} alt="my photo" />
+              <Box>
+                <Name />
+                <Header>
+                  <Contacts />
+                  <TechSkills />
+                  <SoftSkills />
+                </Header>
+              </Box>
+            </Container>
+          </>
+        )}
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Summary />} />
+          <Route path="projects" element={<Experience />} />
+          <Route path="education" element={<Education />} />
+          <Route path="experience" element={<Works />} />
+        </Routes>
+      </div>
+    </ThemeContext.Provider>
   );
 };
